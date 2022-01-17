ca1d7ca34bb9d6e9ada73bf64ae6e97f61e554b6
Dhruvil Dave <dhruvildave45@gmail.com>
2022-01-17 10:57:59 +0530

    feat: mongoose

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://dhruvil:dhruvil123@cluster0.kh6ny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect( CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => app.listen(PORT, () => console.log(`Server running at ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
